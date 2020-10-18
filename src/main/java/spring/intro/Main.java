5b2d62d461e26ff75534b5337e4a74d4ac0e323a
YaTut1901 <59802427+YaTut1901@users.noreply.github.com>
2020-10-18 12:43:44 +0300

    without_review

---
+++ src/main/java/spring/intro/Main.java
@@ -0,0 +1,24 @@
+package spring.intro;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import spring.intro.config.AppConfig;
+import spring.intro.model.User;
+import spring.intro.service.UserService;
+
+public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context
+                = new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = context.getBean(UserService.class);
+
+        User user1 = new User("user1@gmail.com", "password");
+        User user2 = new User("user2@gmail.com", "password");
+        User user3 = new User("user3@gmail.com", "password");
+
+        userService.add(user1);
+        userService.add(user2);
+        userService.add(user3);
+
+        userService.usersList().forEach(System.out::println);
+    }
+}
---
+++ src/main/java/spring/intro/dao/UserDaoImpl.java
@@ -0,0 +1,27 @@
+package spring.intro.dao;
+
+import java.util.List;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import spring.intro.model.User;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public void add(User user) {
+        sessionFactory.openSession().save(user);
+    }
+
+    @Override
+    public List<User> getAll() {
+        return sessionFactory
+                .openSession()
+                .createQuery("from User", User.class)
+                .getResultList();
+    }
+}
